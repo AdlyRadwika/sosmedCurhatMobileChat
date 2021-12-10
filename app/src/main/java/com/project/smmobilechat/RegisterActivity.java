1a826e47d0fc9e02988223f111efea264f41be31
AdlyRadwika <aradwika@gmail.com>
2021-12-10 11:25:18 +0700

    [PROBLEM] register activity 2
    masih belum bisa masuk ke datase hanya percobaan saja seperti modifikasi pda androidmanifest dll

--- app/src/main/java/com/project/smmobilechat/RegisterActivity.java
+++ app/src/main/java/com/project/smmobilechat/RegisterActivity.java
@@ -1,21 +1,17 @@
 package com.project.smmobilechat;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import androidx.appcompat.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.appcheck.FirebaseAppCheck;
-import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.HashMap;
+
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -80,30 +77,31 @@ public class RegisterActivity extends AppCompatActivity {
                             FirebaseUser firebaseUser = auth.getCurrentUser();
                             assert firebaseUser != null;
                             String userid = firebaseUser.getUid();
+                            modelUser user = new modelUser();
 
-
+                            user.setUserName(username);
+                            user.setUserEmail(email);
+                            user.setUserPass(password);
 
                             reference = FirebaseDatabase.getInstance("https://sosmedcurhatmobilechat-166c7-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users").child(userid);
                             //reference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://sosmedcurhatmobilechat-166c7-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
                             HashMap<String, String> hashMap = new HashMap<>();
                             hashMap.put("id",userid);
-                            hashMap.put("username", username);
-                            hashMap.put("imageURL","default");
+                            hashMap.put("username", user.getUserName());
+                            hashMap.put("email", user.getUserEmail());;
+                            hashMap.put("password", user.getUserPass());
 
-                            reference.setValue(hashMap);
-
-
-
-                                        Toast.makeText(RegisterActivity.this, "Account successfully registered", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(intent);
-                                        finish();
-
-
-
-
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(RegisterActivity.this, "Account successfully registered", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
                         } else {
                             Toast.makeText(RegisterActivity.this, "Error"+ task.getException().getMessage(), Toast.LENGTH_LONG).show();
                             System.out.println("Error"+ task.getException().getMessage());
---
+++ app/src/main/java/com/project/smmobilechat/modelUser.java
@@ -0,0 +1,34 @@
+package com.project.smmobilechat;
+
+public class modelUser {
+
+    private String userName;
+    private String userEmail;
+    private String userPass;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+
+}
