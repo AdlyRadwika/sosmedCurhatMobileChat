0d385065c979881d7efcfc96074a16fe42ba46b8
alroyhery <alroy2711@gmail.com>
2021-12-15 16:22:10 +0700

    part 4

--- app/src/main/java/com/project/smmobilechat/MainActivity.java
+++ app/src/main/java/com/project/smmobilechat/MainActivity.java
@@ -1,8 +1,13 @@
 package com.project.smmobilechat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +16,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +25,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.project.smmobilechat.Model.User;
+import com.project.smmobilechat.fragments.ChatsFragment;
+import com.project.smmobilechat.fragments.UsersFragment;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -62,6 +72,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new ChatsFragment(), "Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(), "Users");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
     @Override
@@ -82,5 +104,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return false;
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment (Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
---
+++ app/src/main/java/com/project/smmobilechat/fragments/UsersFragment.java
@@ -0,0 +1,28 @@
+package com.project.smmobilechat.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.smmobilechat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UsersFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UsersFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+}
